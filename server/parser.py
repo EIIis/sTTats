8e022e30e99bbd5e4161a5bb0c735713f2229aea
Ellis Alcantara <ellisalcantara@gmail.com>
2022-02-01 23:42:04 -0600

    Comment counter completed!

---
+++ server/parser.py
@@ -0,0 +1,46 @@
+import json
+
+userData = open('server/user_data.json', 'r')
+
+jsonData = json.load(userData)
+"""
+# Let's me see how many 
+for dataPoints in jsonData:
+    print(dataPoints)
+
+Activity - Having trouble parsing :/
+Ads and data
+App Settings
+Comment - Comment Counter Done 
+Direct Messages
+Profile
+Video
+"""
+
+for info in jsonData['Activity']:
+    for second in jsonData['Activity'][info]:
+        print (second + 'hi')
+        if second is None:
+            print(second + "hmmm yes yes")
+            for third in jsonData['Activity'][info][second]:
+                print(third)
+        else:
+            print(second + 'fuck it\'s not working!')
+            continue
+
+     
+# Functions to count how many comment's you've left!
+def commentCounter():
+    count = 0
+
+    for info in jsonData['Comment']:
+        for second in jsonData['Comment'][info]:
+            for third in jsonData['Comment'][info][second]:
+                print(third)
+                count += 1
+    return count
+
+# print(commentCounter())
+
+
+userData.close()
